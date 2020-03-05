2297f997ae68d84c546c4bee1c37b9c71fc04708
yuankui <yuankui@yuankui-hackintosh.local>
2020-03-05 14:16:27 +0800

    up

---
+++ precache-manifest.911591a2f4d2e6a18e8858a4e2817125.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b43959f76582067d55f04d0189347b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a07e0d93d75a21309f79",
+    "url": "/static/css/main.004bc667.chunk.css"
+  },
+  {
+    "revision": "d6648479c5f85b55f69d",
+    "url": "/static/js/2.06ebd225.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.06ebd225.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a07e0d93d75a21309f79",
+    "url": "/static/js/main.c0ddf906.chunk.js"
+  },
+  {
+    "revision": "8ec958162713c1695355",
+    "url": "/static/js/runtime-main.2ddb808c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
